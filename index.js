2205a2e5b0ac9600280ee927d61f9958f2cdd123
Armando Cruz Cruz <armando.cruz.x.cruz@gmail.com>
2024-11-07 16:57:11 -0600

    test
    test

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ app.put('/api/notes/:id',(request,response) =>{
     response.json(updatedNote)
 })
 
-const PORT=3001
+const PORT= process.env.PORT || 3001
 app.listen(PORT, ()=> {
     console.log(`Server express running on port ${PORT}`);
 })
